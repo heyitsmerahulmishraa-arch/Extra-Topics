4b90fc4a5636ab0316f4164599aa10af094c7746
heyitsmerahulmishraa-arch <heyitsmerahulmishraa@gmail.com>
2026-08-26 16:55:49 +0530

    nodejs added

---
+++ codePractice.js
@@ -0,0 +1,8 @@
+for(let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++){
+        for(let k = 0; k < 5; k++){
+            console.log(`i: ${i}, j: ${j}, k: ${k}`);
+        }
+    }
+    console.log("--------------------");
+}
